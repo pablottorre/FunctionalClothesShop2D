8dc60034f31bf8ee15105854aee155510500a879
pablo Torre <pablo.ttorre@gmail.com>
2023-05-26 19:53:24 -0300

    Offset for the items on the store

--- Assets/_Scripts/StoreManager/StoreTableSale.cs
+++ Assets/_Scripts/StoreManager/StoreTableSale.cs
@@ -15,9 +15,35 @@ public class StoreTableSale : ItemToBuy
     [Header("Item No Longer On Sale")]
     [SerializeField] BoxCollider2D buyZone;
 
+    [Header("Offset For Items")]
+    [SerializeField] private List<Vector3> offsettPosition = new List<Vector3>();
+
     public override void SetItem(ClothesSO _cloth)
     {
         buyZone.enabled = true;
+        switch (_cloth.typeOfClothes)
+        {
+            case ClothesType.Hood:
+                clothesSpriteToDisplay.transform.localPosition = offsettPosition[0];
+                break;
+            case ClothesType.Mask:
+                clothesSpriteToDisplay.transform.localPosition = offsettPosition[1];
+                break;
+            case ClothesType.Torso:
+                clothesSpriteToDisplay.transform.localPosition = offsettPosition[2];
+                break; 
+            case ClothesType.Pelvis:
+                clothesSpriteToDisplay.transform.localPosition = offsettPosition[3];
+                break;
+            case ClothesType.ShoesL:
+                clothesSpriteToDisplay.transform.localPosition = offsettPosition[4];
+                break;
+            case ClothesType.ShoesR:
+                clothesSpriteToDisplay.transform.localPosition = offsettPosition[5];
+                break;      
+            default:
+                break;
+        }
         clothesSpriteToDisplay.sprite = _cloth.clothesSprite;
         float priceToDiscount = Random.Range(0.5f, 0.95f);
         int tempNumb = Mathf.RoundToInt(_cloth.clothesCost * priceToDiscount);
