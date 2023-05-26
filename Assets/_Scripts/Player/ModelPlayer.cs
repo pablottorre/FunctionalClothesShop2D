7eb3537082b523b53d5d6aef97a73f443014b8c4
pablo Torre <pablo.ttorre@gmail.com>
2023-05-26 17:13:47 -0300

    Player now walks

--- Assets/_Scripts/Player/ModelPlayer.cs
+++ Assets/_Scripts/Player/ModelPlayer.cs
@@ -7,21 +7,26 @@ public class ModelPlayer : MonoBehaviour
     [Header("player Stats")]
     [SerializeField] private float _moveSpeed;
     private Rigidbody2D _rb;
+    [SerializeField] Animator _anim;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        UpdateManager.instance.OnUpdateDelegate += OnUpdateDelegate;
-    }
-
-
-    private void OnUpdateDelegate()
-    {
-
     }
 
     public void Move(float horizontal, float vertical)
     {
+        if (horizontal != 0 || vertical != 0)
+        {
+            _anim.SetBool("isWalking", true);
+            _anim.SetFloat("DirX",-horizontal);
+            _anim.SetFloat("DirY", -vertical);
+        }
+        else
+        {
+            _anim.SetBool("isWalking", false);
+        }
+
         Vector2 moveDir = new Vector2(horizontal, vertical);
         moveDir = moveDir.normalized * _moveSpeed * Time.deltaTime;
         _rb.velocity = moveDir;        
